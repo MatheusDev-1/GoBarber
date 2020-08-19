52c7db2b14f178b5abaeec3b9fbbaf8e51a8f0ff
Matheus Oliveira <matheus.mdahora@gmail.com>
2020-08-19 00:09:41 -0300

    Utilização de TypeORM e PostgreSQL

---
+++ src/database/migrations/1597801351726-CreateAppointments.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1597801351726
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+/**
+ * Linha do tempo
+ *
+ * 1º semana: Agendamentos
+ * 2º semana: Usuários
+ * (NOVO DEV) 3º: Edição Agendamentos
+ * 4º semana: Compras
+ *
+ */
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,15 @@
-import { uuid } from 'uuidv4';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
